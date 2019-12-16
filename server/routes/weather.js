7a9196b069f8156eb1adcf26a261cdc4ba1a8a80
Kristin <1358365@DESKTOP-4FH9G41>
2019-12-16 21:52:27 +0300

    postgres connected

--- server/routes/weather.js
+++ server/routes/weather.js
@@ -6,14 +6,11 @@ const API_URL = 'https://api.openweathermap.org/data/2.5/weather?appid=f51bcfb8b
 router.get('/', async (req, res) => {
     const result = await axios.get(API_URL + 'q=' + req.query.city);
     res.json(result.data);
-        // .then(response => res.json(response.data))
-        // .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.get('/coordinates', async (req, res) => {
-    const result = await axios.get(API_URL + 'lon=' + req.query.lon + '&lat=' + req.query.lat)
+    const result = await axios.get(API_URL + 'lon=' + req.query.lon + '&lat=' + req.query.lat);
     res.json(result.data);
-
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
+
 require('dotenv').config();
 
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+var db = require('knex')({
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1',
+        user : '',
+        password : '',
+        database : 'webLab4'
+    }
+});
+
+// App
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
+// App Middleware
 const whitelist = ['http://localhost:3000'];
 const corsOptions = {
     origin: function (origin, callback) {
@@ -21,22 +32,19 @@ const corsOptions = {
     }
 };
 
+app.use(helmet());
 app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(morgan('combined'));
 
+// App Routes - Auth
+//const favouritesRouter = require('./routes/favourites');
+const weatherRouter = require('./routes/weather');
 
-const getWeather= require('./routes/weather');
-app.use('/weather', getWeather);
+//app.use('/favourites', favouritesRouter);
+app.use('/weather', weatherRouter);
 
-const manageFavouriteCities = require('./routes/favouriteCities');
-app.use('/favourites', manageFavouriteCities);
-
-
-const uri = "mongodb://localhost:27017";
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
-const connection = mongoose.connection;
-
-connection.once('open', () => {
-    console.log('MongoDB connection established successfully');
+// App Server Connection
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`app is running on port ${process.env.PORT || 5000}`)
 });
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
